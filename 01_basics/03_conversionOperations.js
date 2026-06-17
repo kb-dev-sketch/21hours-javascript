9b21cc966a6da9751458b34f458b6de300bf5f96
kb-dev-sketch <kb61406@gmail.com>
2026-06-17 16:37:52 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -6,10 +6,39 @@
 //console.log(typeof valueNumber)
 
 //console.log(valueNumber) // but it is a NAN => not a number
-let isloggedIn = 1
-let boolean = Boolean(isloggedIn)
-console.log(boolean)
+//let isloggedIn = 1
+//let boolean = Boolean(isloggedIn)
+//console.log(boolean)
 
 //1 =>true; 0 =>false
 // "" => false
 // "karan" => true
+
+// ----------operations---------------------//
+
+//let value = 3
+//let negative = -value
+//console.log(negative)// -3
+
+//console.log(2 / 3) // 0.6666666
+
+//let str1 = 'hello'
+//let str2 = " hitesh "
+//let str3 = str2 + str1 // hitesh hello
+//console.log(str3)
+
+//console.log(1 + "3") // 13
+//console.log('3' + 45) //345
+//console.log("45" + "56") // 4556
+
+//let a = 3 + "45"
+//console.log(a) // 345
+//console.log(typeof a) // string
+
+//console.log("1" + 2 + 2); //122
+//console.log(1 + (2 + "2")); // 122 use parentesis i code for these type of operations
+//console.log(1 + 2 + "2"); //32 
+console.log(true) // true
+console.log(+true) //1
+console.log(+false) // 0
+console.log(+"");// 0
