18632b0e7f7fe86f546c0a953ab06a41bd50ae23
kb-dev-sketch <kb61406@gmail.com>
2026-06-17 15:12:25 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+
+let password=688848998;
+var username="karan";
+const id=78;
+username="anurag"
+ //let password=123456 //error  password is already declared
+ password=123456
+ //id =78 // error assign to constant variables
+ // console.log([password,username,id]);
+let account; // undefined
+console.log(account)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat allcode as a new version
+
+// alert(3+3) // we are using nodejs ,not browser
+
+// discuss data types
+ // 1.numbers => 2 to power 52
+ // big it 
+ // string
+ // boolean =true ,false 
+ // null =>standalone value
+ // undefined=>
+    // symbol =>unique
+
+ // object 
+
+ //console.log(typeof undefined) // undefined 
+ console.log(typeof undefined) // undefined 
+ console.log(typeof null); // object 
