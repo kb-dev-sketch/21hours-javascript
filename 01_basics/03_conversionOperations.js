7b5cbb2d4c849e7442a6543daac9c7134756d9e5
kb-dev-sketch <kb61406@gmail.com>
2026-06-17 15:40:53 +0000

    conversion of data

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+//let score = "33abc"
+
+//console.log(typeof score) // number
+
+//let valueNumber = Number(score) // but it converts in number
+//console.log(typeof valueNumber)
+
+//console.log(valueNumber) // but it is a NAN => not a number
+let isloggedIn = 1
+let boolean = Boolean(isloggedIn)
+console.log(boolean)
+
+//1 =>true; 0 =>false
+// "" => false
+// "karan" => true
